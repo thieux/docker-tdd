71d25462f095af3a46caea4936983e5c0d3dbfdc
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-08-01 22:51:14 +0200

    Create docker run test fail.
    Should print 'Hello world'.

--- test-image.bash
+++ test-image.bash
@@ -6,4 +6,10 @@ function build() {
   docker build -t 'thieux/hello' .
 }
 
+function run() {
+  docker run 'thieux/hello'
+}
+
 build | grep 'Successfully built' --quiet && ok || fail
+run | grep 'Hello world' --quiet && ok || fail
+
